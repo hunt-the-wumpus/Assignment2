2ed2e3fccbbfb835c919f27c69c054313bfa7c90
Sergei Vorobev <xvorsx@gmail.com>
2016-03-04 23:41:15 -0800

    Add Assignment2 stub code for Fibonacci numbers generator

---
+++ Assignment2Application/Assignment2Application/FibonacciGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2Application
+{
+    public class FibonacciGenerator
+    {
+        /// <summary>
+        /// Return n-th Fibonacci number.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public long Get(int n)
+        {
+            // Enter you code here
+            return 0;
+        }
+    }
+}
--- Assignment2Application/Assignment2Application/Program.cs
+++ Assignment2Application/Assignment2Application/Program.cs
@@ -10,6 +10,20 @@ namespace Assignment2Application
     {
         static void Main(string[] args)
         {
+            var generator = new FibonacciGenerator();
+            Console.WriteLine("Enter n:");
+            string line = Console.ReadLine();
+            int n;
+            if (int.TryParse(line, out n))
+            {
+                long result = generator.Get(n);
+                Console.WriteLine("Result: {0}", result);
+            }
+            else
+            {
+                Console.WriteLine("Cannot parse '{0}'", line);
+            }
+            Console.ReadLine();
         }
     }
 }
