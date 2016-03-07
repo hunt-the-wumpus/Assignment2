18b995c514bb856deb7ae7fb7c21f8d36b815df0
Alexandr <ALEX.Sytsev.2014@yandex.ru>
2016-03-07 18:26:18 +0400

    Added files via upload

---
+++ Assignment2/Assignment2Application/Assignment2Application/FibonacciGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2Application
+{
+    public class FibonacciGenerator
+    {
+        // This class use for get n Fibonacci number
+        public long Get(int n)
+        {
+            long current1 = 1;
+            long current2 = 1;
+            for (int i = 3; i <= n; ++i)
+            {
+                long x = current1 + current2;
+                current1 = current2;
+                current2 = x;
+            }
+            return current2;
+        }
+    }
+}
---
+++ Assignment2/Assignment2Application/Assignment2Application/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new FibonacciGenerator();
+            Console.WriteLine("Enter n:");
+            string line = Console.ReadLine();
+            int n;
+            if (int.TryParse(line, out n))
+            {
+                long result = generator.Get(n);
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Cannot parse " + line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
