040f5c41caf12484ec32c357c2dfb2d6af46233a
prifio <tolya_ash@mail.ru>
2016-03-11 09:31:20 +0300

    Add Get in FibonachiGenerator

--- Assignment2Application/Assignment2Application/FibonacciGenerator.cs
+++ Assignment2Application/Assignment2Application/FibonacciGenerator.cs
@@ -13,10 +13,21 @@ namespace Assignment2Application
         /// </summary>
         /// <param name="n"></param>
         /// <returns></returns>
+        List<long> F;
+        public FibonacciGenerator()
+        {
+            F = new List<long>();
+            F.Add(1);
+            F.Add(1);              
+        }
+
         public long Get(int n)
         {
-            // Enter you code here
-            return 0;
+            if (n < F.Count)
+                return F[n];
+            long res = Get(n - 1) + Get(n - 2);
+            F.Add(res);
+            return res;
         }
     }
 }
