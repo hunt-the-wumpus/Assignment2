a6bbd60cec1398e1f7618892070f690434bf499f
Sergei Vorobev <xvorsx@gmail.com>
2016-03-13 11:21:34 -0700

    Add FibonacciGenerator implementation

--- Assignment2Application/Assignment2Application/FibonacciGenerator.cs
+++ Assignment2Application/Assignment2Application/FibonacciGenerator.cs
@@ -15,8 +15,20 @@ namespace Assignment2Application
         /// <returns></returns>
         public long Get(int n)
         {
-            // Enter you code here
-            return 0;
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            long cur1 = 0;
+            long cur2 = 1;
+            for (int i = 0; i < n - 1; i++)
+            {
+                long temp = cur2;
+                cur2 = cur1 + cur2;
+                cur1 = temp; 
+            }
+            return cur2;
         }
     }
 }
